ddf854de1d52005d31fa9c6b7422ce29ced8796a
Aleksa Savovic <asavovic@griddynamics.com>
2023-05-03 17:30:03 +0200

    controller tests for login

--- src/test/java/asavovic/courseProject/controllers/CustomerControllerTest.java
+++ src/test/java/asavovic/courseProject/controllers/CustomerControllerTest.java
@@ -2,17 +2,23 @@ package asavovic.courseProject.controllers;
 
 import asavovic.courseProject.entities.Customer;
 import asavovic.courseProject.services.CustomerService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +30,7 @@ public class CustomerControllerTest {
     CustomerController customerController;
 
     MockMvc mockMvc;
+
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders
@@ -35,22 +42,22 @@ public class CustomerControllerTest {
     void registrationSuccess() throws Exception {
         String email = "existing@email.com";
         String password = "123";
-        Customer customer = new Customer(email,password);
+        Customer customer = new Customer(email, password);
 
-        when(customerService.register(customer)).thenReturn(new Customer(email,password));
+        when(customerService.register(customer)).thenReturn(new Customer(email, password));
 
         mockMvc.perform(MockMvcRequestBuilders.post("/register")
-                .content("{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}")
-                .contentType("application/json"))
+                        .content("{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}")
+                        .contentType("application/json"))
                 .andExpect(status().isOk());
-        verify(customerService,times(1)).register(any());
+        verify(customerService, times(1)).register(any());
     }
 
     @Test
     public void registerExistingUser() throws Exception {
         String email = "existing@email.com";
         String password = "123";
-        Customer customer = new Customer(email,password);
+        Customer customer = new Customer(email, password);
 
         when(customerService.register(customer)).thenReturn(null);
 
@@ -58,7 +65,43 @@ public class CustomerControllerTest {
                         .content("{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}")
                         .contentType("application/json"))
                 .andExpect(status().isConflict());
-        verify(customerService,times(1)).register(any());
+        verify(customerService, times(1)).register(any());
+    }
+
+    @Test
+    public void login() throws Exception {
+        String email = "existing@email.com";
+        String password = "123";
+        Long sessionId = 1L;
+
+        when(customerService.login(email, password)).thenReturn(sessionId);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                        .content("{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}")
+                        .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        long response = Long.parseLong(content);
+
+        Assertions.assertEquals(response, sessionId);
+
+    }
+
+    @Test
+    public void loginWrongCredentials() throws Exception {
+        String email = "existing@email.com";
+        String password = "123";
+        Long sessionId = 1L;
+
+        when(customerService.login(email, password)).thenReturn(null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                        .content("{\"email\":\"" + email + "\",\"password\":\"" + password + "\"}")
+                        .contentType("application/json"))
+                .andExpect(status().isUnauthorized());
+
     }
 
 }
