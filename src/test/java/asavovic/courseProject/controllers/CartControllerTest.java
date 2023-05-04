8f956c4b45af7ca26dd7f8a187986f87d309debc
Aleksa Savovic <asavovic@griddynamics.com>
2023-05-04 14:19:31 +0200

    show cart controller test added

--- src/test/java/asavovic/courseProject/controllers/CartControllerTest.java
+++ src/test/java/asavovic/courseProject/controllers/CartControllerTest.java
@@ -1,9 +1,12 @@
 package asavovic.courseProject.controllers;
 
+import asavovic.courseProject.entities.Cart;
 import asavovic.courseProject.entities.dto.ProductToAdd;
 import asavovic.courseProject.exceptions.DeficientResourcesException;
 import asavovic.courseProject.services.CartService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,11 +16,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @ExtendWith(MockitoExtension.class)
 public class CartControllerTest {
@@ -46,13 +51,13 @@ public class CartControllerTest {
 
 
         mockMvc.perform(MockMvcRequestBuilders.post("/addProduct")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("sessionId", 2L)
-                .content("{ \"id\": \"" + id + "\", \"amountToAdd\": " + amountToAdd + " }"))
-                        .andExpect(status().isOk());
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("sessionId", 2L)
+                        .content("{ \"id\": \"" + id + "\", \"amountToAdd\": " + amountToAdd + " }"))
+                .andExpect(status().isOk());
 
 
-        verify(cartService, times(1)).addProductToCart(product,2L);
+        verify(cartService, times(1)).addProductToCart(product, 2L);
     }
 
     @Test
@@ -67,6 +72,24 @@ public class CartControllerTest {
                         .header("sessionId", 2L)
                         .content("{ \"id\": \"" + id + "\", \"amountToAdd\": " + amountToAdd + " }"))
                 .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    public void showCart() throws Exception {
+        Long sessionId = 1L;
+        CartDTO cartDTO = new CartDTO();
+        when(cartService.showCart(sessionId)).thenReturn(cartDTO);
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/cart")
+                        .header("sessionId", sessionId))
+                .andReturn();
+        String responseBody = result.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        CartDTO cartResult = mapper.readValue(responseBody, CartDTO.class);
+
+        Assertions.assertEquals(cartDTO.getTotalPrice, cartResult.getTotalPrice);
+        Assertions.assertIterableEquals(cartDTO.getProductDisplay, cartResult.ProductDisplay);
 
     }
 
