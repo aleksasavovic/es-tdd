6d0ade16dc650a9de8536e9bbb6d408783907a68
Aleksa Savovic <asavovic@griddynamics.com>
2023-05-08 17:34:26 +0200

    tUpdateAmountById test added

---
+++ src/test/java/asavovic/courseProject/repositories/ProductRepositoryTest.java
@@ -0,0 +1,34 @@
+package asavovic.courseProject.repositories;
+
+import asavovic.courseProject.entities.Product;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class ProductRepositoryTest {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    @Transactional
+    public void testUpdateAmountById() {
+        Product product = new Product();
+        product.setId(1L);
+        product.setQuantity(10L);
+
+        productRepository.save(product);
+
+        int i = productRepository.updateAmountById(3L, 1L);
+        productRepository.flush();
+
+        Product updatedProduct = productRepository.findById(1L).orElse(null);
+
+        assertNotNull(updatedProduct);
+        assertEquals(Long.valueOf(7L), updatedProduct.getQuantity());
+    }
+}
