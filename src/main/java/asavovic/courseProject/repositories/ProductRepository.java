9626d29842e517a4d53347cb349cdb4bdee94218
Aleksa Savovic <asavovic@griddynamics.com>
2023-05-08 17:50:51 +0200

    FindQuantityById implementation in repository

--- src/main/java/asavovic/courseProject/repositories/ProductRepository.java
+++ src/main/java/asavovic/courseProject/repositories/ProductRepository.java
@@ -7,9 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Modifying(clearAutomatically = true)
     @Query("update Product set quantity = quantity - :amount where id = :id ")
     Integer updateAmountById(@Param("amount") Long amount, @Param("id") Long id);
+
+    @Query("SELECT p.quantity FROM Product p WHERE p.id = :productId")
+    Optional<Long> findQuantityById(@Param("productId") Long productId);
 }
