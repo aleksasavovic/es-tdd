9e67a4bd1eab233fd48542601e0706a49bcaffbc
Aleksa Savovic <asavovic@griddynamics.com>
2023-05-08 17:36:01 +0200

    updateAmountById implemented

--- src/main/java/asavovic/courseProject/repositories/ProductRepository.java
+++ src/main/java/asavovic/courseProject/repositories/ProductRepository.java
@@ -2,8 +2,14 @@ package asavovic.courseProject.repositories;
 
 import asavovic.courseProject.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Modifying(clearAutomatically = true)
+    @Query("update Product set quantity = quantity - :amount where id = :id ")
+    Integer updateAmountById(@Param("amount") Long amount, @Param("id") Long id);
 }
